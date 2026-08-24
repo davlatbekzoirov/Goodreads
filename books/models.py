ae31b0ef10ae692b4eef340830dee8e4717e1ab4
davlatbekzoirov <davlatbekzoirov08@gmail.com>
2026-08-24 17:59:41 +0500

    created BookAuthor model

--- books/models.py
+++ books/models.py
@@ -12,4 +12,9 @@ class Author(models.Model):
     last_name = models.CharField(max_length=100)
     email = models.EmailField()
     bio = models.TextField()
+
+class BookAuthor(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
     
