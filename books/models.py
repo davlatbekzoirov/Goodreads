bc48fecf55832ba11a5d8ba7a3502622694fbd58
davlatbekzoirov <davlatbekzoirov08@gmail.com>
2026-08-24 18:05:03 +0500

    created BookReview model

--- books/models.py
+++ books/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -17,4 +18,10 @@ class BookAuthor(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
-    
+class BookReview(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    comment = models.TextField()
+    stars_given = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
