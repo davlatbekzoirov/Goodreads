d5b713b8b73c5af360f2dae37967ad90c89dc103
davlatbekzoirov <davlatbekzoirov08@gmail.com>
2026-08-24 17:55:55 +0500

    created Book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    isbn = models.CharField(max_length=17)
+
+
+class 
