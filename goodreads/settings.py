ebcd0d11011b255f97672b0c61533b81be958ba5
davlatbekzoirov <davlatbekzoirov08@gmail.com>
2026-08-24 18:26:23 +0500

    update

--- goodreads/settings.py
+++ goodreads/settings.py
@@ -64,11 +64,14 @@ WSGI_APPLICATION = 'goodreads.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'goodreads',
+        'USER': 'postgres',
+        'PASSWORD': 'dave',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
