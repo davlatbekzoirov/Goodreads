3666d93bf5892232a79d5d0d3f73416f71ea943c
davlatbekzoirov <davlatbekzoirov08@gmail.com>
2026-08-26 21:12:47 +0500

    update admin view

--- books/admin.py
+++ books/admin.py
@@ -2,20 +2,38 @@ from django.contrib import admin
 from .models import Book, Author, BookAuthor, BookReview
 
 
+class BookAuthorInline(admin.TabularInline):
+    model = BookAuthor
+    extra = 1
+
+
+class BookReviewInline(admin.TabularInline):
+    model = BookReview
+    extra = 0
+    readonly_fields = ('user', 'stars_given', 'comment')
+
+
+@admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    search_fields = ('title', 'isbn')
     list_display = ('title', 'isbn', 'description')
+    search_fields = ('title', 'isbn')
+    inlines = [BookAuthorInline, BookReviewInline]
 
+
+@admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('first_name', 'last_name', 'email')
+    search_fields = ('first_name', 'last_name', 'email')
 
+
+@admin.register(BookAuthor)
 class BookAuthorAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('book', 'author')
+    search_fields = ('book__title', 'author__first_name', 'author__last_name')
 
+
+@admin.register(BookReview)
 class BookReviewAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Book, BookAdmin)
-admin.site.register(Author, AuthorAdmin)
-admin.site.register(BookAuthor, BookAuthorAdmin)
-admin.site.register(BookReview, BookReviewAdmin)
+    list_display = ('book', 'user', 'stars_given')
+    list_filter = ('stars_given',)
+    search_fields = ('book__title', 'user__username', 'comment')
