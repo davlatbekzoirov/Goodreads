37f2e587f5450f08acee773a3ba79979113b61b0
davlatbekzoirov <davlatbekzoirov08@gmail.com>
2026-08-26 21:54:06 +0500

    update

--- goodreads/urls.py
+++ goodreads/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import landing_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', landing_page)
+    path('users/', include('users.urls'), name='users'),
+    path('', landing_page, name='landing_page')
 ]
--- users/views.py
+++ users/views.py
@@ -4,3 +4,7 @@ from django.views import View
 class RegisterView(View):
     def get(self, request):
         return render(request, "users/register.html")
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, "users/login.html")
