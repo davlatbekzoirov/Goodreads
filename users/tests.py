c6febf7b2e239fb569efa35d40c4df617623b385
davlatbekzoirov <davlatbekzoirov08@gmail.com>
2026-08-30 22:47:39 +0500

    created RegisterTestCase

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+class RegisterTestCase(TestCase):
+    def test_user_account_is_created(self):
+        self.client.post('/users/register', data={
+            'username':'dave', 
+            'first_name':'Dave', 
+            'last_name': 'Zoirov', 
+            'email': 'davlatbekzoirov08@gmail.com',
+            'password': 'dave'
+        })
+
+        user = User.objects.get(username='dave')
+        self.assertEqual(user.first_name, 'Dave')
+        self.assertEqual(user.last_name, 'Zoirov')
+        self.assertEqual(user.email, 'davlatbekzoirov08@gmail.com')
+        self.assertNotEqual(user.password, 'dave')
+        self.assertTrue(user.check_password('dave'))
